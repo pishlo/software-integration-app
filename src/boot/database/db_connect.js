405d57428fc5fe637beedd5f594f0f417f657f92
pishlo <soni.kozaliev@gmail.com>
2025-05-11 01:08:01 +0200

    chore: setup ESLint and Prettier using classic config

--- src/boot/database/db_connect.js
+++ src/boot/database/db_connect.js
@@ -1,5 +1,5 @@
-const pg = require("pg");
-const logger = require("../../middleware/winston");
+const pg = require('pg');
+const logger = require('../../middleware/winston');
 
 const db_config = {
   user: process.env.DB_USER,
@@ -20,16 +20,16 @@ function startConnection() {
 
   db_connection = new pg.Pool(db_config);
 
-  db_connection.connect((err, client) => {
+  db_connection.connect((err, _client) => {
     if (!err) {
-      logger.info("PostgreSQL Connected");
+      logger.info('PostgreSQL Connected');
     } else {
-      logger.error("PostgreSQL Connection Failed");
+      logger.error('PostgreSQL Connection Failed');
     }
   });
 
-  db_connection.on("error", (err, client) => {
-    logger.error("Unexpected error on idle client");
+  db_connection.on('error', (_err, _client) => {
+    logger.error('Unexpected error on idle client');
     startConnection();
   });
 }
--- src/middleware/winston.js
+++ src/middleware/winston.js
@@ -1,25 +1,19 @@
 // const appRoot = require("app-root-path");
-const winston = require("winston");
+const winston = require('winston');
 // define the custom settings for each transport
 const options = {
   file: {
-    level: "info",
+    level: 'info',
     filename: `./logs/app.log`,
     handleException: true,
     maxSize: 5242880, // about 5MB
     maxFiles: 5,
-    format: winston.format.combine(
-      winston.format.timestamp(),
-      winston.format.json()
-    ),
+    format: winston.format.combine(winston.format.timestamp(), winston.format.json()),
   },
   console: {
-    level: "debug",
+    level: 'debug',
     handleException: true,
-    format: winston.format.combine(
-      winston.format.colorize(),
-      winston.format.simple()
-    ),
+    format: winston.format.combine(winston.format.colorize(), winston.format.simple()),
   },
 };
 
@@ -34,7 +28,7 @@ const logger = winston.createLogger({
 
 // create a stream object with a 'write' function
 logger.stream = {
-  write: function (message, encoding) {
+  write: function (message, _encoding) {
     // here we're using the 'info' log level so the output will
     // be picked by both transports (file and console)
     logger.info(message);
