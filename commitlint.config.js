e5f51c639ba90cbcc550f4679dc0c7cbb4fd0ecb
takujoye14 <takuma.joye414@gmail.com>
2025-05-11 11:43:15 +0200

    husky and conventional commits

---
+++ commitlint.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      ['build', 'chore', 'ci', 'docs', 'feat', 'fix', 'perf', 'refactor', 'revert', 'style', 'test'],
+    ],
+  },
+  extends: ['@commitlint/config-conventional'],
+};
